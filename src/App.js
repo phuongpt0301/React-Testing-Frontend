1b00feaa77dcc66f30422a08585f929bcb636cd9
Phan Thanh Phuong <thanhphuong@bonseyjaden.com>
2019-12-29 07:13:57 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReCAPTCHA from "react-google-recaptcha";
 
-function App() {
+import 'antd/dist/antd.css';
+import './App.css';
+import Slider from './Slider';
+
+const App = (props) => {
+
+  const recaptchaRef = React.createRef();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider />
+      <div className="finger-container flex-row">
+        <div className="left">
+          <h4 className="title">Left Thumb</h4>
+          <div className="image-wrapper">
+            <img src={require('./finger-print.png')} alt="finger print left" />
+          </div>
+        </div>
+        <div className="right">
+          <h4 className="title">Right Thumb</h4>
+          <div className="image-wrapper">
+            <img src={require('./finger-print.png')} alt="finger print left" />
+          </div>
+        </div>
+      </div>
+      <div className="captcha-container">
+        <form onSubmit={() => { recaptchaRef.current.execute(); }} >
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            theme="dark"
+            sitekey="Your client site key"
+          />
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Slider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Carousel } from 'antd';
+
+const Slider = () => {
+    return(
+        <Carousel afterChange={onChange}>
+            <div>
+            <h3>1</h3>
+            </div>
+            <div>
+            <h3>2</h3>
+            </div>
+            <div>
+            <h3>3</h3>
+            </div>
+            <div>
+            <h3>4</h3>
+            </div>
+        </Carousel>
+    )
+}
+export default Slider;
